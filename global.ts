5378fa48d3276a666ee7247d9cda9127b792f6b7
牛叠谊 <18715156450@163.com>
2022-07-04 18:17:32 +0800

    feat: update cloudflare config

--- global.ts
+++ global.ts
@@ -4,6 +4,15 @@ const globalConfig = {
         twitter: 'https://twitter.com/0xMatrixMarket',
         medium: 'https://medium.com/matrix-markt',
         discord: 'https://discord.gg/TEpebqaJJF'
+    },
+    cloudflareAssets: {
+        defaultGradientImg: "https://d25xo6hfo9fbc2.cloudfront.net/media/KxWwzATyyEfCBaMnWedNW_IMAGE.png",
+        hotCollectionCardGradientImg: 'https://d25xo6hfo9fbc2.cloudfront.net/media/mPWjEKSye9C0A2vt4u4OW_IMAGE.png',
+        matrixLogoImg: 'https://d25xo6hfo9fbc2.cloudfront.net/media/IxjcM8sug8XJAQJW_fCcc_IMAGE.png',
+        flowNiaBannerBg: 'https://d25xo6hfo9fbc2.cloudfront.net/media/DXLY7JKQSouBcHl6AN-Co_IMAGE.png',
+        flowNiaVideo: 'https://dy8ckr8o2pye7.cloudfront.net/media/flownia.mp4',
+        flowNiaVideoPoster: 'https://dy8ckr8o2pye7.cloudfront.net/media/flownia-video-poster.png',
+        flowNiaARVideo: 'https://dy8ckr8o2pye7.cloudfront.net/media/nft/flownia/AR-effect-rare-2.mp4'
     }
 }
 export default globalConfig;
