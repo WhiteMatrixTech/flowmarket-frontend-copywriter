a422d69829e4a00dbd17cafafe0f919124f7a5bf
牛叠谊 <18715156450@163.com>
2022-08-30 12:03:05 +0800

    feat: update cloudflare baseurl

--- global.ts
+++ global.ts
@@ -6,10 +6,10 @@ const globalConfig = {
         discord: 'https://discord.gg/TEpebqaJJF'
     },
     cloudflareAssets: {
-        defaultGradientImg: "https://d25xo6hfo9fbc2.cloudfront.net/media/KxWwzATyyEfCBaMnWedNW_IMAGE.png",
-        hotCollectionCardGradientImg: 'https://d25xo6hfo9fbc2.cloudfront.net/media/mPWjEKSye9C0A2vt4u4OW_IMAGE.png',
-        matrixLogoImg: 'https://d25xo6hfo9fbc2.cloudfront.net/media/IxjcM8sug8XJAQJW_fCcc_IMAGE.png',
-        flowNiaBannerBg: 'https://d25xo6hfo9fbc2.cloudfront.net/media/DXLY7JKQSouBcHl6AN-Co_IMAGE.png',
+        defaultGradientImg: "https://d8pvwwqlravyw.cloudfront.net/media/KxWwzATyyEfCBaMnWedNW_IMAGE.png",
+        hotCollectionCardGradientImg: 'https://d8pvwwqlravyw.cloudfront.net/media/mPWjEKSye9C0A2vt4u4OW_IMAGE.png',
+        matrixLogoImg: 'https://d8pvwwqlravyw.cloudfront.net/media/IxjcM8sug8XJAQJW_fCcc_IMAGE.png',
+        flowNiaBannerBg: 'https://d8pvwwqlravyw.cloudfront.net/media/DXLY7JKQSouBcHl6AN-Co_IMAGE.png',
         flowNiaVideo: 'https://dy8ckr8o2pye7.cloudfront.net/media/flownia.mp4',
         flowNiaVideoPoster: 'https://dy8ckr8o2pye7.cloudfront.net/media/flownia-video-poster.png',
         flowNiaARVideo: 'https://dy8ckr8o2pye7.cloudfront.net/media/nft/flownia/AR-effect-rare-2.mp4',
